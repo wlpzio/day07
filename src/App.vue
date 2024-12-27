c4c73ea421eb5901b9bd393ef61b38e8a7a93259
韦林鹏 <2796201996@qq.com>
2024-12-27 15:58:08 +0800

    换一个软件

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>根组件
+      - {{ $store.state.title }}
+      - {{ $store.state.count }}
+    </h1>
+    <input type="text" />
+    <Son1></Son1>
+    <hr />
+    <Son2></Son2>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Son1 from './components/Son1.vue'
+import Son2 from './components/Son2.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data: function () {
+    return {}
+  },
   components: {
-    HelloWorld
+    Son1,
+    Son2
+  },
+  created () {
+    // console.log(this.$router)
+    console.log(this.$store)
   }
 }
 </script>
 
-<style lang="less">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 600px;
+  margin: 20px auto;
+  border: 3px solid #ccc;
+  border-radius: 3px;
+  padding: 10px;
 }
 </style>
